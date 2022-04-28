22e97a29dd97e80903a2ba214d17174613d0e062
Walter Cordoba <walterflycba@gmail.com>
2022-04-28 19:12:32 -0300

    Nueva carpeta Controllers y Routes

---
+++ Controllers/productosController.js
@@ -0,0 +1,11 @@
+const productosController= { 
+    index: function {};
+    listado:function{};
+    crear:function{};
+    detalle:function{};
+    detalleComentarios:function{};
+
+
+};
+
+module.exports =productosController;
