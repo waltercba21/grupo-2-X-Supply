79e3085fbdcea5ddbd29837e418610e255541b7f
3LdYn0 <fernando_ruggiero@outlook.com>
2022-05-10 11:13:27 -0300

    Resuelto conflicto de versiones

--- src/Controllers/productsController.js
+++ src/Controllers/productsController.js
@@ -8,10 +8,4 @@ const productsController= {
  },
 }
 
-<<<<<<< HEAD
-module.exports =productsController;
-=======
 module.exports = productsController;
-
-
->>>>>>> 3644939734abf0108d85fde5968ebabc0c628031
--- src/Routes/products.js
+++ src/Routes/products.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const productsController = require ('../Controllers/productsController');
 
 router.get ('/productCart', productsController.productCart);
+
 router.get ('/productDetail', productsController.productDetail);
 
 module.exports = router;
