de097f58eb2e79b92008ea0b9554a48ac6b15cd5
Rene97pf <rene97.pf@gmail.com>
2022-05-17 21:23:15 -0300

    test

--- src/Controllers/productsController.js
+++ src/Controllers/productsController.js
@@ -6,14 +6,11 @@ const productsController= {
    productDetail: (req, res) => {
       res.render ('productDetail')
  },
-<<<<<<< HEAD
 
  admProduct: (req, res) => {
    res.render ('admProduct')
 }
 
-=======
->>>>>>> 79e3085fbdcea5ddbd29837e418610e255541b7f
 }
 
 module.exports = productsController;
