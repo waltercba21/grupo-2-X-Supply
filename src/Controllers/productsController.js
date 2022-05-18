00d9709b2b3e908261f8de728b1430c623c347ef
Rene97pf <rene97.pf@gmail.com>
2022-05-17 23:38:43 -0300

    vista products con json

--- src/Controllers/productsController.js
+++ src/Controllers/productsController.js
@@ -1,14 +1,32 @@
+const fs = require ('fs')
+
+const path = require('path')
+const productsFilePath = path.join(__dirname, "../data/productos.json");
+
+let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'))
+
 const productsController= {
    productCart: (req, res) => {
       res.render ('productCart')
  },
 
    productDetail: (req, res) => {
-      res.render ('productDetail')
+      let id = req.params.id;
+      let product = products.find((product) => product.id == id);
+      res.render ('productDetail', {product})
+
  },
 
- admProduct: (req, res) => {
-   res.render ('admProduct')
+ createProduct: (req, res) => {
+   res.render ('createProduct')
+},
+
+editProduct: (req, res) => {
+   res.render ('editProduct')
+},
+
+products: (req,res) => {
+res.render ('products', {products})
 }
 
 }
--- src/Routes/products.js
+++ src/Routes/products.js
@@ -1,10 +1,14 @@
 const express = require ('express');
 const router = express.Router();
+const multer = requite ('multer')
+const path = require ('path')
 const productsController = require ('../Controllers/productsController');
 
 router.get ('/productCart', productsController.productCart);
 
-router.get ('/productDetail', productsController.productDetail);
-router.get ('/admProduct', productsController.admProduct);
+router.get ('/productDetail/:id', productsController.productDetail);
+router.get ('/editProduct', productsController.editProduct);
+router.get ('/createProduct', productsController.createProduct);
+router.get ('/', productsController.products);
 
 module.exports = router;
