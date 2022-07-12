fceb5d21c6eca9385bb149951bb3f81f552faa8f
Rene97pf <rene97.pf@gmail.com>
2022-07-11 21:01:00 -0300

    rutas arregladas

--- src/Routes/main.js
+++ src/Routes/main.js
@@ -1,11 +1,7 @@
-const express = require ('express');
+const express = require("express");
 const router = express.Router();
-const mainController = require ('../Controllers/mainController');
+const mainController = require("../Controllers/mainController");
 
-router.get ('/', mainController.index);
-
-router.get ('/', mainController.index);
-
-
+router.get("/", mainController.index);
 
 module.exports = router;
--- src/app.js
+++ src/app.js
@@ -1,39 +1,35 @@
-const express= require ("express");
-const session = require ('express-session');
-const path = require ("path");
-const app = express ();
-const methodOverride = require('method-override');
-const mainRouter= require ('./Routes/main');
-const productsRouter = require ('./Routes/products');
-const usersRouter = require ('./Routes/users');
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const app = express();
+const methodOverride = require("method-override");
+const mainRouter = require("./Routes/main");
+const productsRouter = require("./Routes/products");
+const usersRouter = require("./Routes/users");
 
 app.use(express.urlencoded({ extended: false }));
-app.set ('view engine', 'ejs');
-app.set('views','./views');
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use(express.json());
 app.use(methodOverride("_method"));
 
-app.use (session({
-    secret : "Es un secreto",
+app.use(
+  session({
+    secret: "Es un secreto",
     resave: false,
     saveUninitialized: false,
-}));
+  })
+);
 
-app.listen (3000, () => {
-    console.log ("Puerto Activo")
+app.listen(3000, () => {
+  console.log("Puerto Activo");
 });
 
-//Main
-app.use ('/', mainRouter);
-app.use ('/index', mainRouter);
+app.use("/", mainRouter);
 
-//Users
-app.use ('/users', usersRouter);
-app.use ('/', usersRouter);
+app.use("/users", usersRouter);
 
-//Products
-app.use ('/products', productsRouter);
-app.use ('/', productsRouter);
+app.use("/products", productsRouter);
