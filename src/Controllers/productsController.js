a675847fd0773c46c32811b30da89f331fa4a598
Rene97pf <rene97.pf@gmail.com>
2022-07-28 23:13:37 -0300

    back login validationss

--- src/Controllers/productsController.js
+++ src/Controllers/productsController.js
@@ -1,114 +1,119 @@
-const fs = require ('fs');
-let db = require('../database/models');
-const path = require('path');
+const fs = require("fs");
+let db = require("../database/models");
+const path = require("path");
 const productsFilePath = path.join(__dirname, "../data/productos.json");
 
-let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+let products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
-const productsController= {
-	productCart: (req, res) => {
-		res.render ('./products/productCart')
-	},
+const productsController = {
+  productCart: (req, res) => {
+    res.render("./products/productCart");
+  },
 
-	productDetail: (req, res) => {
-		db.Product.findByPk(req.params.id, {
-			include: [{association: 'brand'}, {association: 'category'}]
-		})
-		.then(function(product){
-			res.render('./products/productDetail', {product:product})
-		})
-	},
+  productDetail: (req, res) => {
+    db.Product.findByPk(req.params.id, {
+      include: [{ association: "brand" }, { association: "category" }],
+    }).then(function (product) {
+      res.render("./products/productDetail", { product: product });
+    });
+  },
 
-	createProduct: (req, res) => {
-		let brands = db.Brand.findAll();
-		let category = db.Category.findAll();
-		Promise.all([brands, category])
-		.then(function([brands, category]) {
-			res.render ('./products/createProduct', {brands:brands, category:category})
-		});
-	},
+  createProduct: (req, res) => {
+    let brands = db.Brand.findAll();
+    let category = db.Category.findAll();
+    Promise.all([brands, category]).then(function ([brands, category]) {
+      res.render("./products/createProduct", {
+        brands: brands,
+        category: category,
+      });
+    });
+  },
 
-	store: (req, res) => {
-		let image;
-		if(req.files[0] !=undefined ) {
-			image = req.files[0].filename;
-		}
-		else {
-			image = "default-image.png";
-		}
-		db.Product.create({
-			name: req.body.name,
-			brand_id: req.body.brand,
-			taste: req.body.taste,
-			weight: req.body.weight,
-			price: req.body.price,
-			quantity: req.body.quantity,
-			category_id: req.body.category,
-			utility: req.body.utility,
-			purpose: req.body.purpose,
-			image: image
-		})
-		.then(function(){
-			res.redirect("/products")
-		});
-	},
+  store: (req, res) => {
+    let image;
+    if (req.files[0] != undefined) {
+      image = req.files[0].filename;
+    } else {
+      image = "default-image.png";
+    }
+    db.Product.create({
+      name: req.body.name,
+      brand_id: req.body.brand,
+      taste: req.body.taste,
+      weight: req.body.weight,
+      price: req.body.price,
+      quantity: req.body.quantity,
+      category_id: req.body.category,
+      utility: req.body.utility,
+      purpose: req.body.purpose,
+      image: image,
+    }).then(function () {
+      res.redirect("/products");
+    });
+  },
 
-	editProduct: (req, res) => {
-		let productToEdit =	db.Product.findByPk(req.params.id);
-		let brands = db.Brand.findAll();
-		let category = db.Category.findAll();
-		Promise.all([productToEdit, brands, category])
-		.then(function([product, brands, category]) {
-			res.render ('./products/editProduct', {product:product, brands:brands, category:category})
-		});
-	},
+  editProduct: (req, res) => {
+    let productToEdit = db.Product.findByPk(req.params.id);
+    let brands = db.Brand.findAll();
+    let category = db.Category.findAll();
+    Promise.all([productToEdit, brands, category]).then(function ([
+      product,
+      brands,
+      category,
+    ]) {
+      res.render("./products/editProduct", {
+        product: product,
+        brands: brands,
+        category: category,
+      });
+    });
+  },
 
-	editModif: (req,res) => {
-		let image;
-		if(req.files[0] !=undefined ) {
-			image = req.files[0].filename;
-		}
-		else {
-			image = req.files.image;
-		}
-		db.Product.update({
-			name: req.body.name,
-			brand_id: req.body.brand,
-			taste: req.body.taste,
-			weight: req.body.weight,
-			price: req.body.price,
-			quantity: req.body.quantity,
-			category_id: req.body.category,
-			utility: req.body.utility,
-			purpose: req.body.purpose,
-			image: image
-		}, {
-			where: {
-				idproducts: req.params.id
-			}
-		})
-		.then(function(){
-			res.redirect("/products")
-		});
-	},
-	
-	delete: function (req, res) {
-		db.Product.destroy({
-			where: {
-				idproducts: req.params.id
-			}
-		})
-		.then(function(){
-			res.redirect('/products')
-		});
-	},
+  editModif: (req, res) => {
+    let image;
+    if (req.files[0] != undefined) {
+      image = req.files[0].filename;
+    } else {
+      image = req.files.image;
+    }
+    db.Product.update(
+      {
+        name: req.body.name,
+        brand_id: req.body.brand,
+        taste: req.body.taste,
+        weight: req.body.weight,
+        price: req.body.price,
+        quantity: req.body.quantity,
+        category_id: req.body.category,
+        utility: req.body.utility,
+        purpose: req.body.purpose,
+        image: image,
+      },
+      {
+        where: {
+          idproducts: req.params.id,
+        },
+      }
+    ).then(function () {
+      res.redirect("/products");
+    });
+  },
 
-	list: (req, res) => {
-		db.Product.findAll()
-		.then(function(products){
-			res.render ('./products/products', {products:products})
-		})
-	}
+  delete: function (req, res) {
+    db.Product.destroy({
+      where: {
+        idproducts: req.params.id,
+      },
+    }).then(function () {
+      res.redirect("/products");
+    });
+  },
+
+  list: (req, res) => {
+    db.Product.findAll().then(function (products) {
+      res.render("./products/products", { products: products });
+    });
+  },
 };
 
 module.exports = productsController;
--- src/Controllers/usersController.js
+++ src/Controllers/usersController.js
@@ -1,102 +1,105 @@
-const bcryptjs = require ('bcryptjs');
-const {validationResult} = require ('express-validator');
-const User = require ('../../models/User');
-let db = require('../database/models');
-
-
+const bcryptjs = require("bcryptjs");
+const { validationResult } = require("express-validator");
+const User = require("../../models/User");
+let db = require("../database/models");
 
 const usersController = {
-   register: (req, res) => {
-      db.User.findAll()
-		.then(function(users){
-			res.render ('./users/register', {users:users})
-		}) 
-   },
-   processRegister: async (req,res) =>{
-   const resultValidation = validationResult(req);
-   if (resultValidation.errors.length > 0){
-      return res.render ('./users/register', {
-         errors: resultValidation.mapped (),
-         oldData : req.body
-         });
-   }
-   //Este metodo permite que no se registren dos veces con el mismo email.
-   let userInDB = await db.User.findOne({
-      where: {
-         email: req.body.email
-      }
-   })
-   if (userInDB){ 
-      return res.render ('./users/register',{
-         errors: {
-            email: {
-               msg: 'El usuario ya se encuentra en la base de datos'
-            }
-         },
-         oldData: req.body 
+  register: (req, res) => {
+    db.User.findAll().then(function (users) {
+      res.render("./users/register", { users: users });
+    });
+  },
+  processRegister: async (req, res) => {
+    const resultValidation = validationResult(req);
+    if (resultValidation.errors.length > 0) {
+      return res.render("./users/register", {
+        errors: resultValidation.mapped(),
+        oldData: req.body,
       });
-   }
-   db.User.create({
-		name: req.body.name,
-		lastname: req.body.lastname,
-		email: req.body.email,
-		address: req.body.address,
-		phone: req.body.phone,
-		password : bcryptjs.hashSync(req.body.password,10),
-		image: req.file.filename
-	   })
-	.then(function(){
-		res.redirect("./login")
-	   });
-   },
+    }
+    //Este metodo permite que no se registren dos veces con el mismo email.
+    let userInDB = await db.User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    if (userInDB) {
+      return res.render("./users/register", {
+        errors: {
+          email: {
+            msg: "El usuario ya se encuentra en la base de datos",
+          },
+        },
+        oldData: req.body,
+      });
+    }
+    db.User.create({
+      name: req.body.name,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      address: req.body.address,
+      phone: req.body.phone,
+      password: bcryptjs.hashSync(req.body.password, 10),
+      image: req.file.filename,
+    }).then(function () {
+      res.redirect("./login");
+    });
+  },
 
-   login: (req, res) => {
-      db.User.findAll()
-		.then(function(users){
-			res.render ('./users/login', {users:users})
-		})
-   },
+  login: (req, res) => {
+    db.User.findAll().then(function (users) {
+      res.render("./users/login", { users: users });
+    });
+  },
 
-   loginProcess: (req,res) => {      
-      let userToLogin = db.User.findOne({
-         where: {
-            email: req.body.email
-         }
-      })
-      .then((userToLogin) => {
-         if (userToLogin){
-            let passOk = bcryptjs.compareSync (req.body.password, userToLogin.password)
-         if (passOk){
-            delete userToLogin.password;
-            req.session.userLogged = userToLogin;
-            return res.redirect('./profile/' + userToLogin.idusers)
-         }else {
-            return res.render('./users/login', {
-               errors: {
-                   email: {
-                       msg: 'Credenciales inválidas'
-                   }
-               }
-           });
-         }
-         }
-      })
-   },
+  loginProcess: async (req, res) => {
+    let userToLogin = await db.User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    if (!userToLogin) {
+      return res.render("./users/login", {
+        errors: {
+          email: {
+            msg: "Credenciales inválidas",
+          },
+        },
+      });
+    } else {
+      if (userToLogin) {
+        let passOk = bcryptjs.compareSync(
+          req.body.password,
+          userToLogin.password
+        );
+        if (!passOk) {
+          return res.render("./users/login", {
+            errors: {
+              email: {
+                msg: "Credenciales inválidas",
+              },
+            },
+          });
+        } else {
+          delete userToLogin.password;
+          req.session.userLogged = userToLogin;
+          return res.redirect("./profile/" + userToLogin.idusers);
+        }
+      }
+    }
+  },
 
-   profile: (req, res) => {
-		db.User.findByPk(req.params.id)
-		.then(function(users){
-			res.render('./users/profile', {users:users})
-		})
-	},
+  profile: (req, res) => {
+    db.User.findByPk(req.params.id).then(function (users) {
+      res.render("./users/profile", { users: users });
+    });
+  },
 
-   list: (req, res) => {
-		db.User.findAll()
-		.then(function(users){
-			res.render ('./users/users', {users:users})
-		})
-	}
-
+  list: (req, res) => {
+    db.User.findAll().then(function (users) {
+      res.render("./users/users", { users: users });
+    });
+  },
 };
 
-module.exports= usersController;
+module.exports = usersController;
